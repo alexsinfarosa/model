2a3011560950d4cdfc4e345a6fe3c2b9f462380c
alexsinfarosa <as898@cornell.edu>
2019-04-04 16:21:43 -0400

    management table new content

--- src/components/MoreInfo.jsx
+++ src/components/MoreInfo.jsx
@@ -19,7 +19,20 @@ class MoreInfo extends Component {
     return (
       <div style={{ width: 500 }}>
         <Paper className={classes.root} elevation={4}>
-          <Typography paragraph>Link to extension fact sheets...</Typography>
+          <Typography variant="subheading" gutterBottom>
+            Do you have a blueberry maggot problem?
+          </Typography>
+
+          <p>
+            Follow this link:{" "}
+            <a
+              href="https://cpb-us-e1.wpmucdn.com/blogs.cornell.edu/dist/0/7265/files/2017/01/bbmaggotfly-1v17esy.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              blogs.cornell.edu
+            </a>
+          </p>
         </Paper>
       </div>
     );
