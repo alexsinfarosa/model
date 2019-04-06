e1172668628adeee0ff80a7d2c055ae4f3429098
alexsinfarosa <as898@cornell.edu>
2019-04-06 12:23:29 -0400

    added disclaimer

--- src/components/OutOfSeasonMessage.jsx
+++ src/components/OutOfSeasonMessage.jsx
@@ -4,6 +4,7 @@ import { withStyles } from "@material-ui/core/styles";
 
 import withRoot from "../withRoot";
 import Typography from "@material-ui/core/Typography";
+import NEWALogo from "../assets/newa-logo.svg";
 
 const styles = theme => ({
   root: {
@@ -44,10 +45,29 @@ class OutOfSeasonMessage extends Component {
         >
           PEST MANAGEMENT
         </Typography>
-        <Typography variant="body1" paragraph align="justify">
+
+        <Typography variant="body1" paragraph align="justify" gutterBottom>
           The time of concern for blueberry maggot has passed. The blueberry
           maggot tool will begin again on March 1.{" "}
         </Typography>
+
+        <Typography
+          variant="caption"
+          paragraph
+          align="justify"
+          gutterBottom
+          style={{ color: "rgba(0,0,0,0.9)", lineHeight: 1.7, marginTop: 64 }}
+        >
+          <b>Disclaimer: These are theoretical predictions and forecasts.</b>The
+          theoretical models predicting pest development or disease risk use the
+          weather data collected (or forecasted) from the weather station
+          location. These results should not be substituted for actual
+          observations of plant growth stage, pest presence, and disease
+          occurrence determined through scouting or insect pheromone traps.
+        </Typography>
+
+        <br />
+        <img src={NEWALogo} alt="NEWA logo" style={{ height: 50 }} />
       </div>
     );
   }
